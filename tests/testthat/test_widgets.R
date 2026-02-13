d4fff2fe5256b523aa5ea463ad1971ce1a8a924b
Bjoern Koneswarakantha <erblast2001@gmx.de>
2026-02-13 15:48:16 +0100

    Take out Widget_BarChartSimaerep

--- tests/testthat/test_widgets.R
+++ tests/testthat/test_widgets.R
@@ -13,17 +13,3 @@ test_that("Widget_Simaerep creates a valid HTML widget", {
   expect_true(stringr::str_detect(attr(w, "output_label"), "Simaerep"))
 })
 
-
-test_that("Widget_BarChartSimaerep creates a valid HTML widget", {
-  # Create site list widget
-  w <- Widget_BarChartSimaerep(
-    dfResults = lAnalysis$Analysis_kri0001$Analysis_Flagged,
-    dfGroups = lReport$Reporting_Groups,
-    lMetric = lReport$Reporting_Metrics %>%
-      filter(MetricID == "Analysis_kri0001")
-  )
-
-  expect_s3_class(w, "htmlwidget")
-
-  expect_true(stringr::str_detect(attr(w, "output_label"), "Bar Chart"))
-})
